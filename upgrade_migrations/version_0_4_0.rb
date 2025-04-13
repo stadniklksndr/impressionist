98a267211a5548fc0cff3c451b68d45bd1a463d4
Sasha Stadnyk <stadniklksndr@gmail.com>
2025-04-13 12:09:19 +0300

    fix migrations (#48)

--- upgrade_migrations/version_0_4_0.rb
+++ upgrade_migrations/version_0_4_0.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Migration to add `referrer` column to impressions table
 class Version04UpdateImpressionsTable < ActiveRecord::Migration[4.2]
   def self.up
     add_column :impressions, :referrer, :string
--- upgrade_migrations/version_1_5_2.rb
+++ upgrade_migrations/version_1_5_2.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
+# Migration to add `params` column and index to impressions table
 class Version152UpdateImpressionsTable < ActiveRecord::Migration[4.2]
   def self.up
     add_column :impressions, :params, :text
 
-    add_index :impressions, [:impressionable_type, :impressionable_id, :params],
+    add_index :impressions, %i[impressionable_type impressionable_id params],
               name: "poly_params_request_index", unique: false
   end
 
   def self.down
-    remove_index :impressions, [:impressionable_type, :impressionable_id, :params], name: "poly_params_request_index"
+    remove_index :impressions, %i[impressionable_type impressionable_id params], name: "poly_params_request_index"
     remove_column :impressions, :params
   end
 end
