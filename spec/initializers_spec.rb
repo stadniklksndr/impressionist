982524d46212bcac001574611d1f15b2701c5a4a
Sasha Stadnyk <stadniklksndr@gmail.com>
2025-04-14 09:18:13 +0300

    fix spec rubocops

--- spec/initializers_spec.rb
+++ spec/initializers_spec.rb
@@ -3,14 +3,14 @@
 require "spec_helper"
 
 RSpec.describe "Initializers" do
-  let(:imp) { RUBY_VERSION.match("1.8") ? "is_impressionable" : :is_impressionable }
+  let(:imp) { RUBY_VERSION.match?("1.8") ? "is_impressionable" : :is_impressionable }
 
   it "is extended from ActiveRecord::Base" do
     expect(ActiveRecord::Base).to respond_to(imp)
   end
 
   it "includes methods in ApplicationController" do
-    method = RUBY_VERSION.match("1.8") ? "impressionist" : :impressionist
+    method = RUBY_VERSION.match?("1.8") ? "impressionist" : :impressionist
     expect(ApplicationController).to respond_to(method)
   end
 
